e2955f67ac77df559ff64a6a38490d67f8c29aea
EdilsonBispoCEO <98287629+EdilsonBispoCEO@users.noreply.github.com>
2023-05-18 13:18:31 -0300

    Novas atualizações e exportação com styled_components

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -9,38 +9,30 @@ export const HeroStyle = styled.form`
   display: flex;
   align-items: center;
 
-    &::before {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: var(--cor-principal);
-        content: '';
-        opacity: 0.7;
-    }
+  ::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--cor-principal);
+    content: '';
+    opacity: 0.7;
+  }
 
-    h3{
-        position: relative;
-        color: #eee;
-    }
-
-    @media (max-width: 768px) {
-        height: auto;
-        padding: 24px 0;
-    }
-      
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
   }
 `
-export default HeroStyle
 
-export const HeroTitle = styled.div`
-  h3 {
-    font-family: Gloock, serif;
-  }
+export const HeroTitle = styled.h2`
+  font-family: Gloock, serif;
+  font-size: 48px;
+  position: relative;
+  color: #eee;
+
   @media (max-width: 768px) {
-    h3 {
-      font-size: 2rem;
-    }
+    font-size: 32px;
   }
 `

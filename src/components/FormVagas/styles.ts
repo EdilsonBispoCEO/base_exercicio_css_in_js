9ea1f223b71bf532662e4564f3e1f977e6db3a58
EdilsonBispoCEO <98287629+EdilsonBispoCEO@users.noreply.github.com>
2023-05-18 13:15:54 -0300

    Novas atualizações e exportação com styled_components

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -1,21 +1,15 @@
 import styled from 'styled-components'
 
-export const FormVagasStyle = styled.form`
+export const Formulario = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   background-color: var(--cor-secundaria);
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
-
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-    gap: 20px;
-  }
 `
-export default FormVagasStyle
 
-export const ButtonVagas = styled.button`
+export const BtnPesquisar = styled.button`
   background-color: var(--cor-principal);
   border: 1px solid var(--cor-principal);
   height: 40px;
@@ -24,17 +18,9 @@ export const ButtonVagas = styled.button`
   color: var(--cor-secundaria);
   margin-left: 8px;
   cursor: pointer;
-
-  @media (max-width: 768px) {
-    margin-left: 0px;
-  }
 `
-export const InputVagas = styled.input`
+
+export const Campo = styled.input`
   padding: 0 16px;
   outline-color: var(--cor-principal);
-
-  @media (max-width: 768px) {
-    padding: 10px;
-    margin-bootom: 20px;
-  }
 `
